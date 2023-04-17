59b252b2ca513baceb862ba9c35e241d9380a917
unknown <abhishekparab225@gmail.com>
2023-04-17 12:26:29 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,46 @@
+const Student=[
+    {
+        "id": 0,
+        "Name": "Ram",
+        "Age": "20",
+        "Course": "MERN",
+        "Batch": "October"
+    },
+    {
+        "id": 1,
+        "Name": "Shyam",
+        "Age": "21",
+        "Course": "MERN",
+        "Batch": "November"
+    },
+    {
+        "id": 2,
+        "Name": "Krishna",
+        "Age": "22",
+        "Course": "MERN",
+        "Batch": "September"
+    },
+    {
+        "id": 3,
+        "Name": "Geeta",
+        "Age": "23",
+        "Course": "MERN",
+        "Batch": "September"
+    },
+    {
+        "id": 4,
+        "Name": "Radha",
+        "Age": "24",
+        "Course": "MERN",
+        "Batch": "October"
+    },
+    {
+        "id": 5,
+        "Name": "Shiva",
+        "Age": 25,
+        "Course": "MERN",
+        "Batch": "November"
+    },
+]
+
+export default Student;
